cac3904d14bfea83598a73f99a8fb07da7d84507
bakshi-nb <nitish.bakshi@infosys.com>
2021-08-04 15:18:26 +0530

    changes for logs

--- demo1/src/main/java/com/example/demo/Demo1Application.java
+++ demo1/src/main/java/com/example/demo/Demo1Application.java
@@ -1,16 +1,23 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+
 @RestController
 @SpringBootApplication
 public class Demo1Application {
 
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	
 	@RequestMapping(value="/")
 	public String index() {
+		log.info("Before Execution");
 		return "<h1> Hello World<h1>";
 	}
 	
